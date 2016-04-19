22f584c4a936a3a5248f7252c7869f3b37e6c55c
alexYang <alexyang@gumptech.com>
2016-04-19 17:03:21 +0800

    DDLog add into project

---
+++ alexYangeHello/alexYangeHello/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  alexYangeHello
+//
+//  Created by 杨乐乐 on 16/4/19.
+//  Copyright © 2016年 gamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ alexYangeHello/alexYangeHello/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  alexYangeHello
+//
+//  Created by 杨乐乐 on 16/4/19.
+//  Copyright © 2016年 gamp. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
